1b96452598b12499585231b5d339624ae97c9b86
kalamalahala <solo.driver.bob@gmail.com>
2022-04-21 17:50:22 -0400

    created bootstrap wrapper using LESS

--- admin/partials/twilio-bulk-admin-dashboard.php
+++ admin/partials/twilio-bulk-admin-dashboard.php
@@ -1,6 +1,5 @@
-<div class="bootstrap-wrapper">
 <?php
-echo '<h1>Hello World!</h1>';
+
 /**
  * Provide a admin area view for the plugin
  *
@@ -18,6 +17,27 @@ echo '<h1>Hello World!</h1>';
 
 
 
-    <h1 id="twilio-header">Hello World</h1>
-	<button class="btn btn-primary">Click me</button>
+<div class="bootstrap-wrapper">
+    <form>
+        <h1 id="twilio-header">Hello World</h1>
+        <button class="btn btn-primary">Click me</button>
+        <div class="form-group row">
+            <label for="text" class="col-4 col-form-label">Text Field</label>
+            <div class="col-8">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">
+                            <i class="fa fa-check"></i>
+                        </div>
+                    </div>
+                    <input id="text" name="text" type="text" class="form-control" placeholder="Enter your Twilio SID here...">
+                </div>
+            </div>
+        </div>
+        <div class="form-group row">
+            <div class="col-8">
+                <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+    </form>
 </div>
